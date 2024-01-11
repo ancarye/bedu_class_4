1552ebd6b64da989f7b5e312f9e876c0533cb041
Antonio Romero Rojas <antonio-8000@hotmail.com>
2024-01-10 20:52:20 -0600

    actualizacion 852

--- 01_github_followers.py
+++ 01_github_followers.py
@@ -11,19 +11,41 @@ def get_github_user(username):
     return None
 
 
-
-def download_avatar_user(avatar_url):
+def download_avatar_user(avatar_url, username):
     response = requests.get(avatar_url)
     if response.status_code == 200:
         # Download image
         response_content = response.content
-        filename = 'tmp/avatar.png'
+        filename = f'tmp/{username}.png'
         with open(filename, 'wb') as image:
             image.write(response_content)
             return filename
     return None
 
-username = input('Give me an user name you want to extract the information: ')
+def get_user_folowers(username):
+    url = f'{BASE_URL}users/{username}/followers'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+username = input('Give me an username you want to extract the information:\n')
 selected_user = get_github_user(username)
-print(download_avatar_user(selected_user.get('avatar_url')))
+
+user_folowers = get_user_folowers(username)
+
+for follower in user_folowers:
+    folower_avatar_url = follower.get('avatar_url')
+    folower_username = follower.get('login')
+    download_avatar_user(folower_avatar_url, folower_username)
+
+
+
+
+
+
+
+
+
+
 
