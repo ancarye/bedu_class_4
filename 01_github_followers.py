622ae69661d51c1106b6817a17270b58c56165d6
Antonio Romero Rojas <antonio-8000@hotmail.com>
2024-01-10 20:38:41 -0600

    descarga foto

---
+++ 01_github_followers.py
@@ -0,0 +1,29 @@
+import requests
+
+BASE_URL = 'https://api.github.com/'
+
+def get_github_user(username):
+    url = f'{BASE_URL}users/{username}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+
+
+def download_avatar_user(avatar_url):
+    response = requests.get(avatar_url)
+    if response.status_code == 200:
+        # Download image
+        response_content = response.content
+        filename = 'tmp/avatar.png'
+        with open(filename, 'wb') as image:
+            image.write(response_content)
+            return filename
+    return None
+
+username = input('Give me an user name you want to extract the information: ')
+selected_user = get_github_user(username)
+print(download_avatar_user(selected_user.get('avatar_url')))
+
